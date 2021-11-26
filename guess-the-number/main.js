f72d900e00fd626fc94d7fb6d7fa15f76dc18bdc
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2021-11-26 12:00:47 +0100

    version #1 : red vs blue

--- guess-the-number/main.js
+++ guess-the-number/main.js
@@ -1,4 +1,21 @@
-document.querySelector('input').onchange = () => {
-  const value = document.querySelector('input').value
-  alert(value)
+
+const mysteryNumber = 50
+
+const input = document.querySelector('input')
+
+input.onchange = () => {
+  const value = parseInt(input.value)
+  
+  if (isNaN(value) === true) {
+    alert(value)
+    alert('Saisie invalide, merci de saisir un nombre.')
+    return
+  }
+
+  if (value < mysteryNumber) {
+    document.body.style.backgroundColor = 'red'
+  }
+  if (value > mysteryNumber) {
+    document.body.style.backgroundColor = 'blue'
+  }
 }
